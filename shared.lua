36137f95338c53b45c6ba3c2d3c7f4d494c017b9
x1Taylor1x <x1taylorxbone@gmail.com>
2015-04-20 21:42:23 +0100

    Create shared.lua

---
+++ shared.lua
@@ -0,0 +1,13 @@
+GM.Name = "Sword Art Online"
+GM.Author = "x1Taylor"
+GM.Email = "N/A"
+GM.Website = "N/A"
+
+team.SetUp( 0, "KnightsOfTheBloodOath", Color( 255, 0, 0) )
+team.SetUp( 1, "SleepingKnights", Color( 100, 0, 255) )
+
+function GM.Initialize()
+
+	self.BaseClass.Initialize( self )
+
+end
